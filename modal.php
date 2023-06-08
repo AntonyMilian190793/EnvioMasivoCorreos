ce173666d15e34714613417e292468dd4198f107
Jorge Antony Milian Montalvo <antonymilian19@gmail.com>
2023-06-08 13:21:52 -0500

    08-06-2023

---
+++ modal.php
@@ -0,0 +1,29 @@
+<!-- Modal -->
+
+<div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+
+  <div class="modal-dialog h-100 d-flex flex-column justify-content-center my-0" role="document">
+    <form method="post" id="usuario_form">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h3 class="modal-title">Suscríbete Ahora</h3>
+          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+        <div class="modal-body">
+          <div class="input-group mb-3">
+            <div class="input-group-prepend">
+              <span class="input-group-text" id="basic-addon1">@</span>
+            </div>
+            <input type="email" id="usu_correo" name="usu_correo" class="form-control" placeholder="Correo electrónico" aria-label="Username" aria-describedby="basic-addon1" required>
+          </div>
+          <p> Para regsistrarte en oferta incribles, gratis y otras cosas interesantes, quedese con nosotros y suscribase</p>
+        </div>
+        <div class="modal-footer">
+          <button type="submit" name="action" value="add" id="#" class="btn btn-primary">Enviar</button>
+        </div>
+      </div>
+    </form>
+  </div>
+</div>
