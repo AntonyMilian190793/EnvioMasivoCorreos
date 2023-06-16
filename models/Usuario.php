6c410b6851b3c492307402f1302ef27d470b514a
Jorge Antony Milian Montalvo <antonymilian19@gmail.com>
2023-06-16 09:52:44 -0500

    15-06-2023

--- models/Usuario.php
+++ models/Usuario.php
@@ -6,7 +6,7 @@ class Usuario extends Conectar {
     public function insert_correo($usu_correo){
         $conectar = parent::conexion();
         parent::set_names();
-        $sql ="INSERT INTO tm_usuario (usu_correo, fech_crea, est) VALUES (?, now(), 1)";
+        $sql ="INSERT INTO tm_usuario (usu_correo, rol_id, fech_crea, est) VALUES (?, 1, now(), 1)";
         $sql = $conectar->prepare($sql);
         $sql->bindValue(1, $usu_correo);
         $sql->execute();
